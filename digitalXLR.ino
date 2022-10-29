f65d94f1b79ada48bac407bb14934aec7b346a67
MisterSilvereagle <MisterSilvereagle@gmx.de>
2022-10-29 08:01:53 +0200

    pinMode assignment and send/receive mode

--- digitalXLR.ino
+++ digitalXLR.ino
@@ -1,6 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
+// use library to calculate CRC checks
+#include <CRC.h>
 
+// define clock and data pins
+#define XLR_CLK  2
+#define XLR_DATA 3
+
+#define MODE 0 // 0 = send, 1 = receive
+
+void setup() {
+  // set pin modes depending on mode.
+  #if MODE == 0
+    pinMode(XLR_CLK, OUTPUT);
+    pinMode(XLR_DATA, OUTPUT);
+  #else
+    pinMode(XLR_CLK, INPUT);
+    pinMode(XLR_DATA, INPUT);
+  #endif
+  
+    
 }
 
 void loop() {
